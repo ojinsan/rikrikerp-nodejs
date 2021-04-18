e298ca144d294dc49ead7a24107894abbe7955d8
Fauzan <ojinsyo@gmail.com>
2021-04-18 13:35:16 +0700

    add formating

--- controllers/excel-generator-controller.js
+++ controllers/excel-generator-controller.js
@@ -1741,10 +1741,7 @@ async function createRABSheet(rabsheet, res, TAHUN, RABPB, AHSPs) {
               ":I" +
               secend +
               ") + " +
-              titiksum[m + 1]
-                .map((titik) => "I" + titik + " +")
-                .slice(0, -1)
-                .slice(0, -1),
+              titiksum[m + 1].map((titik) => "I" + titik + " +").slice(0, -2),
           }; //better add result
           rabsheet.getCell("J" + i).value = {
             formula: "=SUM(J" + secstart + ":J" + secend + ")",
